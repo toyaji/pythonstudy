f58c2631d7b0b9ee3aa75e609c83d84a92836c4a
toyaji <happytoday83@naver.com>
2017-01-24 22:12:25 +0900

    20170124 DataCamp Study on Home                BeautifulSoup Extending

--- Datacamp/ImportingWebData/BeautifulSoup.py
+++ Datacamp/ImportingWebData/BeautifulSoup.py
@@ -7,8 +7,17 @@ r = requests.get(url)
 
 html = r.text
 
-soup = BeautifulSoup(html)
+soup = BeautifulSoup(html, 'html.parser')
+
+# lxml 요거를 넣던지 'html.parser' 넣어야지 경고 메세지 않뜨는데 원인은 좀 찾아봐야할듯함...
 
 pretty_soup = soup.prettify()
 
-print(pretty_soup)
+# print(pretty_soup)
+
+print(soup.title)  # 제목부분만 가져오는 명령
+print(soup.get_text())  # text 만 뽑아 오는 방법 (?)
+
+a_tag = soup.find_all('a')  # href 만 뽑아오는방법
+for link in a_tag:
+    print(link.get('href'))
