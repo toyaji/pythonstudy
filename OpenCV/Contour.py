da820440e104a1b58a5f8abc05ff82d660bf2279
toyaji <happytoday83@naver.com>
2017-04-27 18:24:14 +0900

    20170427 OpenCV Contour Area, etc

--- OpenCV/Contour.py
+++ OpenCV/Contour.py
@@ -1,5 +1,6 @@
 from os.path import expanduser
 import cv2
+import numpy as np
 
 def contour(image):
     img = cv2.imread(image)
@@ -78,12 +79,58 @@ def approximation(image):
     cv2.destroyAllWindows()
 
 
-def
+def convex(image):
+    img = cv2.imread(image, cv2.IMREAD_GRAYSCALE)
+    img1 = img.copy()
+    ret, thr = cv2.threshold(img, 127, 255, 0)
+    _, contours, _ = cv2.findContours(thr, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+
+    cnt = contours[1]
+    cv2.drawContours(img, [cnt], 0, (0, 255, 0), 3)
+
+    # 볼록한 형태 contour 인지 확인함
+    check = cv2.isContourConvex(cnt)
+
+    if not check:
+        hull = cv2.convexHull(cnt)
+        cv2.drawContours(img1, [hull], 0, (255, 0, 0), 3)
+        cv2.imshow('convexhull', img1)
+        cv2.waitKey(0)
+
+    cv2.imshow('contour', img)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
+def minArea(image):
+    img = cv2.imread(image, cv2.IMREAD_GRAYSCALE)
+    ret, thr = cv2.threshold(img, 200, 255, 0)
+    _, contours, _ = cv2.findContours(thr, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+
+    print(contours[1].__str__)
+    contours = contours.sort(order='y')
+    cnt = max(contours, key=cv2.contourArea)
+
+
+    x, y, w, h = cv2.boundingRect(cnt)
+    cv2.rectangle(img, (x, y), (x+w, y+h), (140, 0, 255), 4)
+
+    rect = cv2.minAreaRect(cnt)
+    box = cv2.boxPoints(rect)
+    box = np.int0(box)  # 위에 나온 좌표가  float 로 나오니까 int로 바꿔주는거임
+
+    cv2.drawContours(img, [box], 0, (0, 255, 0), 2)
+    cv2.imshow('rectangle', img)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
 
 if __name__ == '__main__':
     image = expanduser(r'~\pictures\paul.jpg')
+    image2 = expanduser(r'\pictures\kadisoka-free-font.jpg')
     # image = cv2.resize(image, None, fx=0.2, fy=0.2, interpolation=cv2.INTER_AREA)
     contour(image)
     moment(image)
     contourArea(image)
     approximation(image)
+    convex(image)
+    minArea(image)
